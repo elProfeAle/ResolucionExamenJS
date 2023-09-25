a45bd90a9db6650619446bf7e7d0c9e832222b8e
octubre1100 <37826866@ivt.edu.ar>
2023-09-25 11:01:16 -0300

    Clase condicional

---
+++ claseCondicional.js
@@ -0,0 +1,29 @@
+//Condicional 
+let notaAlumno = Number(prompt('Ingresa la nota'))
+
+if (notaAlumno >= 8)
+{
+    alert('Estás aprobado')
+
+}else {
+    alert('Estas desaprobado')
+}
+
+let estoyEnLaLista = true 
+
+if (estoyEnLaLista == false)
+{
+    alert('No paso al XV')
+}else{
+    alert('Entro al XV')
+}
+
+let energia = Number(prompt('Ingresa la cantidad de voltios de energia electrica'))
+
+if(energia >= 100)
+{
+    alert('Podés prender la luz y otros dispositivos')
+}else{
+    alert('necesitas un poco mas de energía')
+}
+
